fe2656439c40d710d44265fab70664875afc4546
Adrian Castillo <david.marcanao07@gmail.com>
2023-09-05 19:15:32 -0300

    upload

--- Proyecto1/Proyecto1/urls.py
+++ Proyecto1/Proyecto1/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Proyecto1.view import saludo, fecha
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saludo/', saludo),
+    path('fecha/', fecha),
 ]
